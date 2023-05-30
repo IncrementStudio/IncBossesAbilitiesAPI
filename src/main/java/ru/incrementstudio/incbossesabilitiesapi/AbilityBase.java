a093faaa5741c7984515e84f21dfba163ba840ed
SUHARIK <sukhoveev.roma@gmail.com>
2023-05-30 15:31:24 +1000

    Add annotations

--- src/main/java/ru/incrementstudio/incbossesabilitiesapi/AbilityBase.java
+++ src/main/java/ru/incrementstudio/incbossesabilitiesapi/AbilityBase.java
@@ -1,25 +1,31 @@
 package ru.incrementstudio.incbossesabilitiesapi;
 
 import org.bukkit.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AbilityBase {
     protected String abilityName, boss, phase, configName;
+    @NotNull
     public String getCapabilityName() { return abilityName; }
+    @NotNull
     public String getBoss() { return boss; }
+    @NotNull
     public String getPhase() { return phase; }
+    @NotNull
     public String getConfigName() { return configName; }
 
     protected boolean active;
+    @NotNull
     public boolean isActive() { return active; }
 
-    public AbilityBase(String boss, String phase, String configName) {
+    public AbilityBase(@NotNull String boss, @NotNull String phase, @NotNull String configName) {
         this.abilityName = AbilityExtension.getInstance().getName().replace("IncBossesAbility_", "");
         this.boss = boss;
         this.phase = phase;
         this.configName = configName;
     }
 
-    public abstract void start(LivingEntity bossEntity);
+    public abstract void start(@NotNull LivingEntity bossEntity);
 
     public abstract void stop();
 }
--- src/main/java/ru/incrementstudio/incbossesabilitiesapi/AbilityExtension.java
+++ src/main/java/ru/incrementstudio/incbossesabilitiesapi/AbilityExtension.java
@@ -1,12 +1,13 @@
 package ru.incrementstudio.incbossesabilitiesapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AbilityExtension extends JavaPlugin {
     private static AbilityExtension instance;
     public static AbilityExtension getInstance() { return instance; }
-
-    public abstract AbilityBase getAbility(String boss, String phase, String capability);
+    @NotNull
+    public abstract AbilityBase getAbility(@NotNull String boss, @NotNull String phase, @NotNull String capability);
 
     @Override
     public void onEnable() {
