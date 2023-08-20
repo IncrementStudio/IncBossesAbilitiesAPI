85781a34698129697e1508ae9c7ca1a2dec7d4be
SUHARIK <sukhoveev.roma@gmail.com>
2023-08-20 14:27:56 +1000

    Make ability bases full abstract

--- src/main/java/ru/incrementstudio/incbosses/AbilityExtension.java
+++ src/main/java/ru/incrementstudio/incbosses/AbilityExtension.java
@@ -1,15 +1,9 @@
 package ru.incrementstudio.incbosses;
 
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.plugin.java.JavaPlugin;
 import ru.incrementstudio.incbosses.bosses.Boss;
 import ru.incrementstudio.incbosses.bosses.phases.Phase;
 
-public abstract class AbilityExtension extends JavaPlugin {
-    private static AbilityExtension instance;
-    public static AbilityExtension getInstance() { return instance; }
-    public abstract AbilityBase getAbility(Boss boss, Phase phase, ConfigurationSection config);
-    public void onEnable() {
-        instance = this;
-    }
+public interface AbilityExtension {
+    AbilityBase getAbility(Boss boss, Phase phase, ConfigurationSection config);
 }
