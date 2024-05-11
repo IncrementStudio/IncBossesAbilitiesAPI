60d999f4336cdfa918b8afd9fe2aa75550d21eee
Jone501 <sukhoveev.roma@gmail.com>
2024-05-11 19:08:10 +1000

    [!] AbilityPlugin now is abstract

--- src/main/java/ru/incrementstudio/incbosses/api/AbilityPlugin.java
+++ src/main/java/ru/incrementstudio/incbosses/api/AbilityPlugin.java
@@ -3,29 +3,21 @@ package ru.incrementstudio.incbosses.api;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.PluginClassLoader;
 
 import java.util.logging.Logger;
 
 // @formatter:off
 public abstract class AbilityPlugin {
-    private final Logger logger;
-    public Logger getLogger() { return logger; }
-
-    protected final String name;
-    public String getName() {
-        return name;
-    }
+    public static Plugin getIncBosses() { return null; }
+    public Logger getLogger() { return null; }
+    public String getName() { return null; }
 
     protected final Boss boss;
     protected final Phase phase;
     protected final FileConfiguration bossConfig;
     protected final ConfigurationSection abilityConfig;
-    public static Plugin IncBossesPlugin = ((PluginClassLoader) AbilityPlugin.class.getClassLoader().getParent()).getPlugin();;
 
     public AbilityPlugin(Boss boss, Phase phase, FileConfiguration bossConfig, ConfigurationSection abilityConfig) {
-        logger = Logger.getLogger(AbilityPlugin.class.getName());
-        this.name = getClass().getClassLoader().getName();
         this.boss = boss;
         this.phase = phase;
         this.bossConfig = bossConfig;
