9638b248867433b7f4272fb3e7a63c94e0962995
Jone501 <sukhoveev.roma@gmail.com>
2024-05-11 19:23:46 +1000

    [!] Add onLoad method

--- src/main/java/ru/incrementstudio/incbosses/api/AbilityPlugin.java
+++ src/main/java/ru/incrementstudio/incbosses/api/AbilityPlugin.java
@@ -9,16 +9,17 @@ import java.util.logging.Logger;
 // @formatter:off
 public abstract class AbilityPlugin {
     public static Plugin getIncBosses() { return null; }
-    public Logger getLogger() { return null; }
-    public String getName() { return null; }
+    public final Logger getLogger() { return null; }
+    public final String getName() { return null; }
 
-    public AbilityPlugin(String name, Boss boss, Phase phase, FileConfiguration bossConfig, ConfigurationSection abilityConfig) {}
+    public AbilityPlugin(String name, Boss boss, Phase phase, FileConfiguration bossConfig, ConfigurationSection abilityConfig) { }
 
+    public void onLoad() { }
     public void start(StartReason reason) { }
     public void stop(StopReason reason) { }
 
-    public Boss getBoss() { return null; }
-    public Phase getPhase() { return null; }
-    public FileConfiguration getBossConfig() { return null; }
-    public ConfigurationSection getAbilityConfig() { return null; }
+    public final Boss getBoss() { return null; }
+    public final Phase getPhase() { return null; }
+    public final FileConfiguration getBossConfig() { return null; }
+    public final ConfigurationSection getAbilityConfig() { return null; }
 }
